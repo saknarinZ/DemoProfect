0ccb300b0aeaa6fa4801036a05b9039a34dbe52d
saknarin <saknarin.css57@ubru.ac.th>
2020-09-09 16:35:33 +0700

    โครงโปรเจค 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component'
+
 const routes: Routes = [
   {
-    path: 'home', component: HomeComponent
-  }
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((C) => C.HomeModule),
+  },
 ];
 
 @NgModule({
